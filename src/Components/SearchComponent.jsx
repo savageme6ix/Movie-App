2bb51f24f47945c83a86e4fe796bdd75ad75a278
savageme6ix <maingithomas803@gmail.com>
2026-03-30 14:02:09 +0300

    Created a searchComponent

---
+++ src/Components/SearchComponent.jsx
@@ -0,0 +1,13 @@
+
+const SearchComponent = () => {
+  return (
+    <div className="searchContainer">
+        <input 
+            placeholder="Search a movie"
+        />
+        <button>Search</button>
+    </div>
+  )
+}
+
+export default SearchComponent
