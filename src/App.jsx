ece050adbee4c74b7253ab51d27f96cf4e91d465
savageme6ix <maingithomas803@gmail.com>
2026-03-30 17:39:50 +0300

    Set up Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-
+import { Routes,Route } from "react-router-dom"
+import Home from "./Home"
 const App = () => {
   
   return (
-    
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   )
 }
 
