28168deb5c382e732ec9c412343c486f3d00ffad
savageme6ix <maingithomas803@gmail.com>
2026-03-30 15:29:39 +0300

    Setted up an async fetch for the Movie data

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,23 @@ import SearchComponent from "./Components/SearchComponent";
 import MovieCard from "./Components/MovieCard";
 
 const App = () => {
+  async function fetchMovie() {
+    try{
+      const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_TMDB_KEY}`);
+
+      if(!response.ok){
+        const message = `An error occurred: ${response.status}`
+        throw new Error (message);
+      }
+      const data = await response.json();
+      console.log(data);
+
+    } catch (error){
+      console.error("Fetch error" , error.message)
+    }
+  }
+  fetchMovie();
+
   return (
     <div>
       <Header />
