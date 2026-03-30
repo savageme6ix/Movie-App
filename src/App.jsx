c11d75b8abedc7f77d130e0e25cde8eabf669724
savageme6ix <maingithomas803@gmail.com>
2026-03-30 14:09:48 +0300

    Imported the components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import Header from "./Components/Header";
+import SearchComponent from "./Components/SearchComponent";
+import MovieCard from "./Components/MovieCard";
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Header />
+      <SearchComponent />
+      <MovieCard />
+    </div>
   )
 }
 
