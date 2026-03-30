4b9cc0f4da176b4b4c982cd909410101f220e52a
savageme6ix <maingithomas803@gmail.com>
2026-03-30 14:05:16 +0300

    Created a Header component

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+  return (
+    <div className='headerContainer'>
+        <h4>Movie App</h4>
+        <a>Home</a>
+        <a>Favourites</a>
+    </div>
+  )
+}
+
+export default Header
