456cceb9ccf9788cde303f9f62fb67fe756b4175
savageme6ix <maingithomas803@gmail.com>
2026-03-30 13:59:10 +0300

    Created a folder for componets and created a movie card component

---
+++ src/Components/MovieCard.jsx
@@ -0,0 +1,15 @@
+
+const MovieCard = () => {
+  return (
+    <div className="card">
+        <section>
+            <img />
+        </section>
+        <footer>
+            <h5></h5>
+        </footer>
+    </div>
+  )
+}
+
+export default MovieCard
